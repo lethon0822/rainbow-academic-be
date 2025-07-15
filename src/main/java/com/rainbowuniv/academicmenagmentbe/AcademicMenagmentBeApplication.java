d03a32ffbb9e03adcf0980a948192835bee7fae4
1ethon0822 <1ethyjj9240@gmail.com>
2025-07-15 15:28:01 +0900

    academic-management

---
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/AcademicMenagmentBeApplication.java
@@ -0,0 +1,13 @@
+package com.rainbowuniv.academicmenagmentbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicMenagmentBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicMenagmentBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rainbowuniv/academicmenagmentbe/AcademicMenagmentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainbowuniv.academicmenagmentbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicMenagmentBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
