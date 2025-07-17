47a0c33e1243c78a911f90c7c725088bfe18d43c
yongwa1214 <yongwa1214@gmail.com>
2025-07-17 13:07:13 +0900

    강ì강의 등록, 등록 강의 보기ì 수정|| 강의 계íì(계획서) 수정 완료

--- src/main/java/com/rainbowuniv/academicmenagmentbe/professor/ProfessorController.java
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/professor/ProfessorController.java
@@ -1,9 +1,11 @@
 package com.rainbowuniv.academicmenagmentbe.course;
 
 
+import com.rainbowuniv.academicmenagmentbe.common.util.HttpUtils;
 import com.rainbowuniv.academicmenagmentbe.course.model.CourseGetReq;
 import com.rainbowuniv.academicmenagmentbe.course.model.CourseGetRes;
 import com.rainbowuniv.academicmenagmentbe.course.model.CoursePostReq;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -13,21 +15,24 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/rainbowuniv.ac.kr/course")
+@RequestMapping("/api/professor")
 public class CourseController {
     private final CourseService courseService;
 
-    @PostMapping
-    public int saveCourse(@RequestBody CoursePostReq req){
-        int user = 10001;
-        req.setUserId(user);
+    //강의 등록
+    @PostMapping("/course")
+    public int saveCourse(HttpServletRequest httpReq, @RequestBody CoursePostReq req){
+        int userId = (int)HttpUtils.getSessionValue(httpReq, "userId");
+        req.setUserId(userId);
         int result = courseService.saveCourse(req);
         return result;
     }
-    @GetMapping
-    public List<CourseGetRes> findMyCourse(CourseGetReq req){
-        int user = 10001;
-        req.setUserId(user);
+
+    //신청 현황보기
+    @GetMapping("")
+    public List<CourseGetRes> findMyCourse(HttpServletRequest httpReq,CourseGetReq req){
+        int userId = (int)HttpUtils.getSessionValue(httpReq, "userId");
+        req.setUserId(userId);
         List<CourseGetRes> result = courseService.findMyCourse(req);
         return result;
     }
