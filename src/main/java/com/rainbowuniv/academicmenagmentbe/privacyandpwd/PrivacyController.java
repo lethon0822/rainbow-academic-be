a2bd1315a8b778a222c2355f8534b8c2dc0657be
1ethon0822 <1ethyjj9240@gmail.com>
2025-07-18 16:53:25 +0900

    작업중

--- src/main/java/com/rainbowuniv/academicmenagmentbe/privacyandpwd/PrivacyController.java
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/privacyandpwd/PrivacyController.java
@@ -4,18 +4,19 @@ import com.rainbowuniv.academicmenagmentbe.privacyandpwd.model.PrivacyPutReq;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/accont")
+@RequestMapping("/api/account")
 public class PrivacyController {
     private final PrivacyService privacyService;
 
-    @PutMapping
-    public int update (PrivacyPutReq req) {
+    @PutMapping("/privacy")
+    public int update (@RequestBody PrivacyPutReq req) {
         return privacyService.updateMyPrivacy(req);
     }
 }
