ca53c2b00b3173af6cc4b5047b17fccf863ec506
mood7dev <mood7dev@gmail.com>
2025-07-23 21:41:45 +0900

    이사 많이 감

--- src/main/java/com/rainbowuniv/academicmenagmentbe/grade/GradeController.java
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/grade/GradeController.java
@@ -5,6 +5,8 @@ import com.rainbowuniv.academicmenagmentbe.grade.model.GradeReq;
 import com.rainbowuniv.academicmenagmentbe.grade.model.GradeSearchReq;
 import com.rainbowuniv.academicmenagmentbe.lectures.model.LecturesEvaluationDto;
 import com.rainbowuniv.academicmenagmentbe.professor.ProfessorService;
+import com.rainbowuniv.academicmenagmentbe.profile.ProfileService;
+import com.rainbowuniv.academicmenagmentbe.profile.model.ProfileDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,47 +19,9 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class GradeController {
-
     private final GradeService gradeService;
     private final ProfessorService professorService;
 
-    // 성적조회(간소) - 학생용
-    @GetMapping("/student/rank")
-    public List<GradeDTO> getSimpleGrades(@ModelAttribute GradeSearchReq searchReq) {
-        return gradeService.findBySimpleGrade(searchReq);
-    }
-
-    // 성적 전체 조회
-    @GetMapping("/student/grade/all")
-    public ResponseEntity<?> getGrades(
-            @RequestParam Integer userId,
-            @RequestParam(required = false) Integer courseId,
-            @RequestParam(required = false) Integer semester,
-            @RequestParam(required = false, defaultValue = "grades") String type) {
-
-        if (userId == null) {
-            return ResponseEntity.badRequest().body("userId is required");
-        }
-
-        if (courseId == null) {
-            courseId = -1;
-        }
-
-        if (semester == null) {
-            semester = -1;
-        }
-
-        switch (type) {
-            case "grades":
-                return ResponseEntity.ok(gradeService.getGradesBySubject(userId, courseId, semester));
-            case "category":
-                return ResponseEntity.ok(gradeService.getCreditByCategory(userId, courseId, semester));
-            case "semester":
-                return ResponseEntity.ok(gradeService.getSemesterGrades(userId, courseId, semester));
-            default:
-                return ResponseEntity.badRequest().body("Invalid type");
-        }
-    }
 
     // 성적 등록 및 수정 - 교수용
     @PutMapping("/professor/course/grade")
@@ -66,13 +30,7 @@ public class GradeController {
         return ResponseEntity.ok("성적 등록 완료");
     }
 
-    // 강의 평가 학생용
-    @PostMapping("/student/course/survey")
-    public ResponseEntity<String> studentSurvey(@RequestBody LecturesEvaluationDto dto) {
-        int result = professorService.studentSurvey(dto);
-        if (result == 1) {
-            return ResponseEntity.ok("강의 평가가 성공적으로 등록되었습니다.");
-        }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("강의 평가 등록에 실패했습니다.");
-    }
+
+
+
 }
