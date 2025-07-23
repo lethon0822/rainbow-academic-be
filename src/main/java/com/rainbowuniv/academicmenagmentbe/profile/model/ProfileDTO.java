7ed2f8ee0012f867dbb14a4a1bcbbc755ba85f68
mood7dev <mood7dev@gmail.com>
2025-07-23 21:41:45 +0900

    이사 많이 감

---
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/profile/model/ProfileDTO.java
@@ -0,0 +1,16 @@
+package com.rainbowuniv.academicmenagmentbe.profile.model;
+
+import lombok.Data;
+
+@Data
+public class ProfileDTO {
+    private String loginId;
+    private String userName;
+    private String email;
+    private String userRole;
+    private String professorName;
+    private String status;
+    private Integer grade;
+    private Integer semester;
+    private String deptName;
+}
