20e1d27742049c77817a2dcef9068dabd2c9c5d0
taewon1918 <taewon1918@gmail.com>
2025-07-23 13:04:23 +0900

    교수 => 출결관리, 성적관리 세잎 07.23

--- src/main/java/com/rainbowuniv/academicmenagmentbe/enrollmentgrade/model/EnrollmentGradePutReq.java
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/enrollmentgrade/model/EnrollmentGradePutReq.java
@@ -5,7 +5,10 @@ import lombok.Data;
 @Data
 public class EnrollmentGradePutReq {
     private int enrollmentId; // 수강신청 ID
+    private Double grade; // 총점 예: 95
+    private String rank; // 등급 (rank) (예: A+)
     private Integer midScore; // 중간
     private Integer finScore; // 기말
-    private String grade; // 등급 (rank) (예: A+)
+    private Integer attendanceScore; // 출석
+    private Integer assignmentScore; // 과제
 }
