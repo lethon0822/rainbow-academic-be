56a81a1c4ffd2c6047d9fbc3d8ec60944cf2c22a
yongwa1214 <yongwa1214@gmail.com>
2025-07-24 12:02:19 +0900

    ë[docs]DELETE ãGradeReq

--- src/main/java/com/rainbowuniv/academicmenagmentbe/grade/GradeController.java
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/grade/GradeController.java
@@ -1,14 +1,8 @@
 package com.rainbowuniv.academicmenagmentbe.grade;
 
-import com.rainbowuniv.academicmenagmentbe.grade.model.GradeDTO;
-import com.rainbowuniv.academicmenagmentbe.grade.model.GradeReq;
-import com.rainbowuniv.academicmenagmentbe.grade.model.GradeSearchReq;
-import com.rainbowuniv.academicmenagmentbe.lectures.model.LecturesEvaluationDto;
+
 import com.rainbowuniv.academicmenagmentbe.professor.ProfessorService;
-import com.rainbowuniv.academicmenagmentbe.profile.ProfileService;
-import com.rainbowuniv.academicmenagmentbe.profile.model.ProfileDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,12 +17,7 @@ public class GradeController {
     private final ProfessorService professorService;
 
 
-    // 성적 등록 및 수정 - 교수용
-    @PutMapping("/professor/course/grade")
-    public ResponseEntity<String> updateGrade(@RequestBody GradeReq req) {
-        gradeService.updateGrade(req);
-        return ResponseEntity.ok("성적 등록 완료");
-    }
+
 
 
 
