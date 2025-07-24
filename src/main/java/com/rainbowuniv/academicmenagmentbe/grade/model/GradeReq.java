5759fb4db313ee2819e32cdcb118f869bd97f832
yongwa1214 <yongwa1214@gmail.com>
2025-07-24 11:56:08 +0900

    rollback start

--- src/main/java/com/rainbowuniv/academicmenagmentbe/grade/model/GradeReq.java
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/grade/model/GradeReq.java
@@ -1,8 +1,10 @@
 package com.rainbowuniv.academicmenagmentbe.grade.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class GradeReq {
     private int enrollmentId;
     private Integer score;
