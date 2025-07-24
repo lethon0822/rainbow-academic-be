ddf0015825b99dc9c63bb5b6d9ca3e6e5f8f47ef
taewon1918 <taewon1918@gmail.com>
2025-07-24 11:38:19 +0900

    성적기입 작업중

---
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/enrollmentgrade/model/GradePutReq.java
@@ -0,0 +1,18 @@
+package com.rainbowuniv.academicmenagmentbe.enrollmentgrade.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class GradePutReq {
+    private int midScore;
+    private int finScore;
+    private int attendanceScore;
+    private int assignmentScore;
+    private String rank;
+    private int enrollmentId;
+}
