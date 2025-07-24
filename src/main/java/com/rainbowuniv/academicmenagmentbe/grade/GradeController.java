571e5fc75b25ca5a94e7dd1e0988f7b9bc13982f
yongwa1214 <yongwa1214@gmail.com>
2025-07-24 12:05:48 +0900

    update

--- src/main/java/com/rainbowuniv/academicmenagmentbe/grade/GradeController.java
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/grade/GradeController.java
@@ -3,14 +3,14 @@ package com.rainbowuniv.academicmenagmentbe.grade;
 
 import com.rainbowuniv.academicmenagmentbe.professor.ProfessorService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class GradeController {
     private final GradeService gradeService;
