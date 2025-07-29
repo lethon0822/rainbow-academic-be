bd1d04f0704488f2d115cd49e3106c733a533e7d
1ethon0822 <1ethyjj9240@gmail.com>
2025-07-29 18:12:45 +0900

    ì 수정 작업

--- src/main/java/com/rainbowuniv/academicmenagmentbe/privacyandpwd/PrivacyController.java
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/privacyandpwd/PrivacyController.java
@@ -1,7 +1,9 @@
 package com.rainbowuniv.academicmenagmentbe.privacyandpwd;
 
+import com.rainbowuniv.academicmenagmentbe.common.util.HttpUtils;
 import com.rainbowuniv.academicmenagmentbe.privacyandpwd.model.PrivacyGetRes;
 import com.rainbowuniv.academicmenagmentbe.privacyandpwd.model.PrivacyPutReq;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +16,19 @@ import org.springframework.web.bind.annotation.*;
 public class PrivacyController {
     private final PrivacyService privacyService;
 
-    @GetMapping("/privacy/{userId}")
-    public ResponseEntity<?> select (@RequestParam int userId) {
+    @GetMapping("/privacy")
+    public ResponseEntity<?> select (HttpServletRequest req) {
+        int userId= (int) HttpUtils.getSessionValue(req, "userId");
         PrivacyGetRes result = privacyService.selectMyPrivacy(userId);
         return ResponseEntity.ok(result);
     }
 
     @PutMapping("/privacy")
-    public ResponseEntity<?> update (@RequestBody PrivacyPutReq req) {
-        int result = privacyService.updateMyPrivacy(req);
-        return ResponseEntity.ok(result);
+    public ResponseEntity<?> update (HttpServletRequest httpReq, @RequestBody PrivacyPutReq req) {
+        int result = (int) HttpUtils.getSessionValue(httpReq, "userId");
+        req.setUserId(result);
+        log.info("으어어어억{}", result);
+        int result2 = privacyService.updateMyPrivacy(req);
+        return ResponseEntity.ok(result2);
     }
 }
