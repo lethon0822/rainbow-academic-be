a43c859be5aaf2cd011d6c18ecce620787fae684
1ethon0822 <1ethyjj9240@gmail.com>
2025-08-28 15:11:34 +0900

    -

--- src/main/java/com/rainbowuniv/academicmenagmentbe/application/ApplicationMapper.java
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/application/ApplicationMapper.java
@@ -1,7 +1,11 @@
 package com.rainbowuniv.academicmenagmentbe.application;
 
+import com.rainbowuniv.academicmenagmentbe.application.model.AppGetRes;
 import com.rainbowuniv.academicmenagmentbe.application.model.AppPostReq;
+import com.rainbowuniv.academicmenagmentbe.application.model.AppPutReq;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ApplicationMapper {
---
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/application/model/AppPutReq.java
@@ -0,0 +1,11 @@
+package com.rainbowuniv.academicmenagmentbe.application.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AppPutReq {
+    private int appId;
+    private String status;
+}
