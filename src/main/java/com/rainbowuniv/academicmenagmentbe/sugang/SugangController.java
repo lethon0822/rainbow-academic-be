1e87c5c29a6a96d406ae743043db1963072b94f8
ongsoony8382 <ongsoony8382@gmail.com>
2025-08-29 15:54:54 +0900

    금학기 수강신청 내역 조회 완료

--- src/main/java/com/rainbowuniv/academicmenagmentbe/sugang/SugangController.java
+++ src/main/java/com/rainbowuniv/academicmenagmentbe/sugang/SugangController.java
@@ -34,24 +34,10 @@ public class SugangController {
 
     // 금학기 수강 신청한 과목들 전체 조회
     @GetMapping("current")
-    public ResponseEntity<?> findAllMySugangCourses(HttpServletRequest httpReq, currentMySugangListReq req) {
+    public ResponseEntity<?> findAllMySugangCourses(HttpServletRequest httpReq, @ModelAttribute currentMySugangListReq req) {
 
         // 유저 아이디 세션 처리
         int userId = (int) HttpUtils.getSessionValue(httpReq, "userId");
-
-        // 테스트할 때 currentDateTime 조작해보기 위함. 테스트 때는 true로 바꾸고 진행.
-        boolean testMode = true;
-        int currentDateTime;
-        if (testMode) {
-            currentDateTime = 20251012; //yyyyMMdd 하드 코딩
-        }else { //테스트 모드가 아닐 때는 현재 실제 시간 반영
-            currentDateTime = Integer.parseInt(
-                    LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
-            );
-        }
-        int currentSemesterId = sugangService.findCurrentSemesterId(currentDateTime);
-        req.setCurrentSemesterId(currentSemesterId);
-
 
         List<MySugangListRes> mySugangListRes = sugangService.findAppliedCoursesByUserId(userId, req);
         return ResponseEntity.ok().body(mySugangListRes);
